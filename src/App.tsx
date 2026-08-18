b368bc76d592703d42997919a3107e64285d397a
Jules Docx <jules.docx@gmail.com>
2026-08-18 16:03:15 +0200

    first working build

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
 import heroImg from './assets/hero.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <section id="center">
         <div className="hero mt-12">
-          <img src={heroImg} className="base" width="300" height="300" alt="" />
+          <img src={heroImg} className="base" width="250" height="250" alt="" />
         </div>
         <div>
           <h1>Koicuts</h1>
@@ -17,7 +15,16 @@ function App() {
            Under <code> 🚧 construction </code> 
           </p>
         </div>
-
+        <div className="buttons">
+          <div className="button">
+            <a id="ghent" href="https://koicutsgent.youcanbook.me/?utm_source=ig&utm_medium=website">Booking Ghent</a>
+            <label htmlFor="ghent">Ghent @purble___palace </label>
+          </div>
+          <div className="button">
+            <a id="brussels" href="https://koicutsbrussel.youcanbook.me/?utm_source=ig&utm_medium=website">Booking Brussels</a>
+            <label htmlFor="brussels">Brussels @canape.bx </label>
+          </div>
+        </div>
       </section>
 
       <div className="ticks"></div>
