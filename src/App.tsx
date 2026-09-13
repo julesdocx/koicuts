6acd59924bac646e411862bd1f40b1376b64c6f0
Jules Docx <jules.docx@gmail.com>
2026-09-13 16:31:56 +0200

    styling and new hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import heroImg from './assets/hero.svg'
+import heroImg from './assets/eline_exports.jpeg'
 import './App.css'
 
 function App() {
@@ -6,25 +6,34 @@ function App() {
   return (
     <>
       <section id="center">
-        <div className="hero mt-12">
-          <img src={heroImg} className="base" width="250" height="250" alt="" />
-        </div>
         <div>
           <h1>Koicuts</h1>
-          <p>
-           Under <code> 🚧 construction </code> 
-          </p>
         </div>
-        <div className="buttons">
-          <div className="button">
-            <a id="ghent" href="https://koicutsgent.youcanbook.me/?utm_source=ig&utm_medium=website">Booking Ghent</a>
-            <label htmlFor="ghent">Ghent @purble___palace </label>
-          </div>
-          <div className="button">
-            <a id="brussels" href="https://koicutsbrussel.youcanbook.me/?utm_source=ig&utm_medium=website">Booking Brussels</a>
-            <label htmlFor="brussels">Brussels @canape.bx </label>
-          </div>
+        <div className="hero mt-12">
+          <img src={heroImg} className="base" width="350" height="350" alt="" />
         </div>
+      <div className="buttons">
+          <label htmlFor="ghent">Ghent @purble___palace </label>
+        <div className="button">
+          <a id="ghent" className="cta-btn" href="https://koicutsgent.youcanbook.me/?utm_source=ig&utm_medium=website">
+            <span className="cta-btn__outer">
+              <span className="cta-btn__inner">
+                <span>Booking <b>Ghent</b></span>
+              </span>
+            </span>
+          </a>
+        </div>
+          <label htmlFor="brussels">Brussels @canape.bx </label>
+        <div className="button">
+          <a id="brussels" className="cta-btn" href="https://koicutsbrussel.youcanbook.me/?utm_source=ig&utm_medium=website">
+            <span className="cta-btn__outer">
+              <span className="cta-btn__inner">
+                <span>Booking <b>Brussels</b></span>
+              </span>
+            </span>
+          </a>
+        </div>
+      </div>
       </section>
 
       <div className="ticks"></div>
